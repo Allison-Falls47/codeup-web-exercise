da8cc77864e930481d636591d28df87d7efb1bdd
Allison Falls <allison.falls2@gmail.com>
2021-05-27 12:46:54 -0400

    prework done

---
+++ js/external.js
@@ -0,0 +1,13 @@
+"use strict";
+
+// ... code
+
+console.log("Hello World");
+console.log("Hello Polaris");
+
+//alert("Hello Folks")
+var confirmed = confirm('Are you sure you want to do XYZ?');
+console.log(confirmed); // will be either true or false
+
+var userInput = prompt('Please type something:');
+console.log('The user entered: ' + userInput);
