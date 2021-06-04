5c908759def2c432c8f78f480dd7f63a8b950548
Allison Falls <allison.falls2@gmail.com>
2021-06-04 12:09:37 -0400

    excercise 2

---
+++ js/for_loops.js
@@ -0,0 +1,19 @@
+function showMultiplicationTable(x){
+    for(let y = 1; y<= 10; y++) {
+        console.log(x + " x " + y + " = " + (x*y))
+    }
+}
+
+console.log(showMultiplicationTable(7))
+
+
+
+for (let ev = 0; ev<=10; ev++) {
+    random = ()(Math.random()* (200 + 1));}{
+    if (ev % 2 !== 0) {
+        console.log(ev + " is odd");}
+        else {
+            console.log(ev + " is even")
+        }
+
+}
---
+++ js/loop_classwork.js
@@ -0,0 +1,12 @@
+
+var message = "";
+
+//var howmany = prompt("how many?");
+
+for (let index = 0; index <= 10; index++) {
+    message = message + "index is: " + index + "\n";
+}
+
+alert(message);
+
+alert(index);
