f69dd0ebc3480bebf21346da79876a9895e55671
Allison Falls <allison.falls2@gmail.com>
2021-06-04 17:57:59 -0400

    Finished do while loop

--- js/while.js
+++ js/while.js
@@ -1,6 +1,28 @@
+"use strict";
 var b = 2;
 do{
     console.log(b);
     b*= 2;
 }
 while (b < 65537);
+
+//space
+
+let totalCanSell =Math.ceil(Math.random() * (100-50) + 50);
+console.log("The number of cones I can sell" + totalCanSell);
+
+//total sold today
+let totalSold = 0;
+
+do {
+    let conesBought = Math.ceil(Math.random() * (5 - 1) + 1);
+
+    if ((totalSold + conesBought) > totalCanSell){
+        console.log("I'm sorry, I can't sell " + conesBought);
+        continue;
+    }
+    console.log("my customer bought" + conesBought);
+    totalSold = totalCanSell + conesBought;
+    console.log("I've sold this many" + totalSold);
+
+} while (totalSold < totalCanSell);
