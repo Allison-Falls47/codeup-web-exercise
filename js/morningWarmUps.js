72671b5af244f5abe1dfd88f6155f8e8b89c789c
Allison Falls <allison.falls2@gmail.com>
2021-07-07 10:23:51 -0400

    finished morning excercises

--- js/morningWarmUps.js
+++ js/morningWarmUps.js
@@ -29,3 +29,52 @@ function capitalizeFirstLetter(input){
         return input.charAt(0).toUpperCase() + input.slice(1);
 }
 console.log(capitalizeFirstLetter(["joe", "jim", "jane"]))
+
+
+
+// July 7th 2021
+// TODO: Create a method on the object down below named getFullName that when called returns the users full name using the "this" key term.
+// Example: personOne.getFullName() ---- returns ----> "silvia floopertan"
+var personOne = {
+    firstName: "silvia",
+    lastName: "floopertan",
+    ageInYears: 34,
+    heightInInches: 65
+}
+
+personOne.getFullName = function(){
+    console.log(personOne.firstName + " " + personOne.lastName)
+};
+personOne.getFullName();
+
+
+
+// TODO: Create a method on the object down below named getNicelyFormattedFullName that when called returns the
+//  users full name with both first letters in the names being capitalized look at example down below for more reference.
+// Example: personOne.getNicelyFormattedFullName() ---- returns ----> "Dan Valdarez"
+var personTwo = {
+    firstName: "dan",
+    lastName: "valdarez",
+    ageInYears: 61,
+    heightInInches: 73
+}
+
+personTwo.getNicelyFormattedFullName = function(){
+    console.log(personTwo.firstName.charAt(0).toUpperCase() + personTwo.firstName.slice(1) + " " + personTwo.lastName.charAt(0).toUpperCase()+ personTwo.lastName.slice(1))
+};
+personTwo.getNicelyFormattedFullName ();
+
+
+// TODO: Create a method on the object below that gives us the behavior that the example implies.
+// Example: personThree.intro() ---- returns ----> "Hello, My name is Arlene Martinez and I am 25 years old"
+var personThree = {
+    firstName: "arlene",
+    lastName: "martinez",
+    ageInYears: 25,
+    heightInInches: 62
+}
+
+personThree.intro = function(){
+    console.log(personThree.firstName + " " + personThree.lastName)
+};
+personThree.intro();
