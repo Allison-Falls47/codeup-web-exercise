a281fe5e62eb23a22dc7eadf1d2c7b9d35df9609
Allison Falls <allison.falls2@gmail.com>
2021-07-09 09:43:39 -0400

    done

--- js/objectAndArray.js
+++ js/objectAndArray.js
@@ -54,3 +54,69 @@ var students = [
 ]
 
 console.log(whoIsPassing(students));
+
+function dateStringToObject(string){
+    var splitString = (string.split("-"))
+    return {month: splitString[1], day: splitString[2], year: splitString[0]}
+}
+
+console.log(dateStringToObject("2016-2-13"));
+
+console.log(dateStringToObject("2005-11-29"));
+
+function reverseString(string){
+    var revString = ""
+    for ( let i = string.length -1; i >= 0; i--) {
+        revString += string.charAt(i);
+    }
+    return revString
+}
+
+function numberInfo(number){
+    var isNumberPositive = (number > 0);
+    var isNumberEven = (number % 2 === 0);
+    var isNumberZero = (number === 0);
+
+return {
+    isPositive: isNumberPositive,
+    isEven: isNumberEven,
+    isZero: isNumberZero
+}
+
+}
+console.log(numberInfo(-1));
+console.log(numberInfo(6));
+console.log(numberInfo(0));
+
+function removedDuplicates(arrayOfStrings){
+    return [...new Set(arrayOfStrings)]
+}
+
+console.log(removedDuplicates(["bob","squid","samantha","bob"]));
+console.log(removedDuplicates(["tilda","tilda"]));
+
+function mostOccuringNumber(numbers) {
+    var counts = [];
+    for (let i = 0; i < numbers.length; i++) {
+        var aNumber = numbers[i].toString();
+        if (counts[aNumber] === undefined){
+            counts[aNumber] = 1;
+    }
+else{
+        counts[aNumber]++;
+    }
+    }
+    var maxCount = 0;
+    var winningNumber = "";
+    for (const countsKey in counts){
+        console.log(" count element " + countsKey + " has a tally of " + counts[countsKey]);
+        var currentCount = counts[countsKey]
+        if(currentCount > maxCount) {
+            maxCount = currentCount;
+            winningNumber = countsKey;
+        }
+    }
+    return winningNumber;
+}
+
+console.log(mostOccuringNumber([8,93,28,8,27,-62,32,8,-62]));
