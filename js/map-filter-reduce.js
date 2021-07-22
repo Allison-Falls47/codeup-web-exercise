3b2a98653069a6cf3a4c053a7f1a0c02147f8aa3
Allison Falls <allison.falls2@gmail.com>
2021-07-22 16:16:35 -0400

    5

---
+++ js/map-filter-reduce.js
@@ -0,0 +1,58 @@
+const users = [
+    {
+        id: 1,
+        name: 'ryan',
+        email: 'ryan@codeup.com',
+        languages: ['clojure', 'javascript'],
+        yearsOfExperience: 5
+    },
+    {
+        id: 2,
+        name: 'luis',
+        email: 'luis@codeup.com',
+        languages: ['java', 'scala', 'php'],
+        yearsOfExperience: 6
+    },
+    {
+        id: 3,
+        name: 'zach',
+        email: 'zach@codeup.com',
+        languages: ['javascript', 'bash'],
+        yearsOfExperience: 7
+    },
+    {
+        id: 4,
+        name: 'fernando',
+        email: 'fernando@codeup.com',
+        languages: ['java', 'php', 'sql'],
+        yearsOfExperience: 8
+    },
+    {
+        id: 5,
+        name: 'justin',
+        email: 'justin@codeup.com',
+        languages: ['html', 'css', 'javascript', 'php'],
+        yearsOfExperience: 9
+    }
+];
+
+const threeLong = users.filter( users => users.languages.length > 2)
+console.log(threeLong)
+
+const mapFit = users.map(users => users.email);
+console.log(mapFit)
+
+const totalExp = users.reduce((total, person) => {
+    return total + person.yearsOfExperience;
+}, 0);
+console.log(totalExp)
+
+const averageExp = users.reduce((total, person) => {
+    return totalExp / person.id;
+}, 0);
+console.log(averageExp)
+
+const longEm = users.reduce((largest, person) => {
+   return largest.length < person.email.length
+},0)
+console.log(longEm)
